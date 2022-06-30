fd7445d19d771eeb67c2481a33070f594a15f18c
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2022-06-29 20:07:37 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import { WebGL } from './WebGL.js';
+import WebGL from 'three/examples/jsm/capabilities/WebGL.js';
 import { Viewer } from './viewer.js';
-import { SimpleDropzone } from '../node_modules/query-string/index.js'
+import { SimpleDropzone } from 'simple-dropzone';
 import { ValidationController } from './validation-controller.js';
-import { queryString } from '../node_modules/query-string/index.js';
+import queryString from 'query-string';
 
 if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
   console.error('The File APIs are not fully supported in this browser.');
